c03b213a55d4536798290053a340f5ee5f779de2
Joshua Averion <joshaver24@gmail.com>
2021-06-03 00:33:12 -0400

    Completed Roverlist

--- RoverList/Program.cs
+++ RoverList/Program.cs
@@ -11,6 +11,117 @@ namespace RoverList
         static void Main(string[] args)
         {
             RoverList<string> list = new RoverList<string>();
+            Console.WriteLine("What words would you like to add to the list? Type 'done' when you are done.");
+            // TODO:  Implement the RoverList class
+            // TODO:  Create a RoverList and then fill it with 16 words
+            Console.WriteLine(" ");
+            list.Add("lebron");
+            list.Add("hello");
+            list.Add("bingbong");
+            list.Add("cake");
+            list.Add("cheese");
+            list.Add("pizza");
+            list.Add("mom");
+            list.Add("joe");
+            list.Add("banana");
+            list.Add("playstation");
+            list.Add("cool");
+            list.Add("the");
+            list.Add("when");
+            list.Add("where");
+            list.Add("rover");
+            list.Add("red");
+            Console.WriteLine(" ");
+
+            // TODO:  Print out the list
+            list.ListNodes();
+            
+            // TODO:  Prompt the user to input words, add those words to the list until they enter the word "done"
+            while (true)
+            {
+                
+                String prompt = System.Console.ReadLine();
+                if (prompt.Equals("done"))
+                {
+                    break;
+                }
+                else
+                {
+                    list.Add(prompt);
+                }
+
+            }
+            Console.WriteLine(" ");
+
+            // TODO:  Print out the list
+            list.ListNodes();
+
+            Console.WriteLine(" ");
+
+            Console.WriteLine("What words would you like to add to the FRONT of the list? Type 'done' when you are done.");
+            
+            Console.WriteLine(" ");
+
+            // TODO:  Prompt the user to input words, add those words to the FRONT of the list until they enter the word "done"
+            while (true)
+            {
+
+                String prompt1 = System.Console.ReadLine();
+                if (prompt1.Equals("done"))
+                {
+                    break;
+                }
+                else
+                {
+                    list.Add(0, prompt1);
+                }
+
+            }
+            Console.WriteLine(" ");
+
+            // TODO:  Print out the list
+            list.ListNodes();
+            Console.WriteLine(" ");
+
+            // TODO:  Remove every word with an odd length
+            Console.WriteLine("Press 'enter' to remove every word with an odd length.");
+            System.Console.ReadLine();
+
+            int listLength = list.Counter();
+
+            for (int i = listLength - 1; i >= 0; i--)
+            {
+
+                string currentWord = list.ElementAt(i);
+                if (currentWord.Length % 2 == 1)
+                {
+
+                    list.RemoveAt(i);
+                }
+            }
+
+            Console.WriteLine("");
+
+            // TODO:  Print out the list
+            list.ListNodes();
+            Console.WriteLine("");
+
+            // TODO:  Clear the list
+            Console.WriteLine("Press 'enter' to clear the list.");
+
+            System.Console.ReadLine();
+
+            list.Clear();
+
+            Console.WriteLine("");
+
+            // TODO:  Print out the list
+            list.ListNodes();
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Enter to execute the program. Have a nice day:)");
+            System.Console.ReadLine();
 
             // TODO:  Implement the RoverList class
             // TODO:  Create a RoverList and then fill it with 16 words
--- RoverList/RoverList.cs
+++ RoverList/RoverList.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Diagnostics;
 namespace RoverList
 {
     public class RoverList<T> : RoverListBase<T>
     {
         // Add any variables you need here
-        
+        private int count;
+        public bool Empty
+        {
+            get { return this.count == 0;  }
+        }
         public RoverList ()
         {
 
@@ -19,32 +23,138 @@ namespace RoverList
 
         public override void Add(T data)
         {
-            throw new NotImplementedException();
+            Node word = new Node(data);
+            word.Next = null;
+
+            current = head;
+            if (current == null)
+            {
+                head = word;
+                return;
+            }
+
+            while (current.Next != null)
+            {
+                current = current.Next;
+            }
+
+            current.Next = word;
+            return;
         }
 
         public override void Add(int Position, T data)
-        {
-            throw new NotImplementedException();
+        { 
+            Node word = new Node(data);
+            current = head;
+            if (Position == 0)
+            {
+                head = word;
+                word.Next = current;
+                return;
+            }
+            
+            for (int i = 0; i < Position - 1; i++)
+            { 
+                current = current.Next;
+            }
+
+            word.Next = current.Next;
+            current.Next = word;
+
+            return;
         }
 
         public override void Clear()
         {
-            throw new NotImplementedException();
+            head = null;
         }
 
         public override T ElementAt(int Position)
         {
-            throw new NotImplementedException();
+
+            if (Position < 0 || Position >= this.Counter())
+            {
+                Console.WriteLine("Invalid position");
+                return default(T);
+            }
+
+            current = head;
+            if (Position == 0)
+            {
+                return current.Data;
+            }
+
+            for (int i = 0; i < Position; i++)
+            {
+                current = current.Next;
+            }
+
+            return current.Data;
         }
 
         public override void ListNodes()
-        {
-            throw new NotImplementedException();
+        { 
+            current = head;
+            if (head == null)
+            {
+                Console.WriteLine("List is empty");
+            } else
+            {
+                while (current.Next != null)
+                {
+                    Console.WriteLine(current.Data);
+                    current = current.Next;
+                }
+                Console.WriteLine(current.Data);
+            }
         }
 
         public override bool RemoveAt(int Position)
         {
-            throw new NotImplementedException();
+            current = head;
+            object result = null;
+
+            if (Position == 0)
+            {
+                result = current.Data;
+                head = current.Next;
+                return true;
+            }
+            else
+            {
+                for (int i = 0; i < Position - 1; i++)
+                    current = current.Next;
+
+                result = current.Next.Data;
+
+                current.Next = current.Next.Next;
+            }
+
+            return true;
         }
+
+        public int Counter()
+        {
+
+            int count = 0;
+
+            current = head;
+            if (current == null)
+            {
+                Console.WriteLine("List is empty");
+                return count;
+            }
+
+            while (current.Next != null)
+            {
+                count++;
+                current = current.Next;
+            }
+
+            count++;
+
+            return count;
+        }
+
     }
 }
